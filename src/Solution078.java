c668a18cddf948549cb4417259524b52ca247789
Henray0607 <1084632143@qq.com>
2020-06-21 15:29:13 +0800

    78. 子集

---
+++ src/Solution078.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution078 {
+    public static List<List<Integer>> subsets(int[] nums) {
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(subsets(nums));
+    }
+}
