f731003034f359a9a172387f6ebf9b77331bc452
Henray0607 <1084632143@qq.com>
2020-06-22 22:19:54 +0800

    104. 二叉树的最大深度

---
+++ src/Solution104.java
@@ -0,0 +1,20 @@
+public class Solution104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
